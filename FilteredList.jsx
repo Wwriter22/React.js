1e0784d511d52717495c0fd8c24dba1bc0aa8e0b
Wwriter22 <90862725+Wwriter22@users.noreply.github.com>
2022-02-04 21:51:12 -0700

    Add files via upload
    Added all of the new solutions to a filtered list so that I can organize the items between fruits, vegetables, and everything

--- FilteredList.jsx
+++ FilteredList.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { DropdownButton, MenuItem} from 'react-bootstrap';
+import { DropdownButton, Dropdown} from 'react-bootstrap';
 import List from './List';
 
 class FilteredList extends Component {
     constructor(props) {
         super(props);
-        // TODO: Add a new key/value pair in the state to keep track of type
         this.state = {
-            search: ""
+            search: "",
+            type: "",
         };
     }
     // Sets the state whenever the user types on the search bar
@@ -17,20 +17,31 @@ class FilteredList extends Component {
 
     filterItem = (item) => {
         // Checks if the current search term is contained in this item
-        // TODO: Add condition to check item’s type
-        return item.name.toLowerCase().search(this.state.search) !== -1;
+        if(item.type == this.state.type){
+           return item.name;
+        }else if (this.state.type == ""){
+            return item.name;
+        }
+
+
     }
-    /* TODO: Add an event handling method for when an item in dropdown is selected
-       Per the DropdownButton documentation, this function should take in an eventKey and
-       event
-    */
+
+    button = (eventKey, event) => {
+        if(eventKey == "All"){
+            this.setState({type:""});
+        }else{
+            this.setState({type: eventKey })
+        }
+    }
     render() {
         return (
             <div className="filter-list">
                 <h1>Produce Search</h1>
                 {/* TODO: Add more menu items with onSelect handlers*/}
-                <DropdownButton id="typeDropdown" title={"Type"}>
-                    <MenuItem eventKey="all" onSelect={HANDLER FUNCTION HERE}>All</MenuItem>
+                <DropdownButton id="typeDropdown" title={"Type"} onSelect={this.button}>
+                    <Dropdown.Item eventKey="All">All</Dropdown.Item>
+                    <Dropdown.Item eventKey="Fruit">Fruit</Dropdown.Item>
+                    <Dropdown.Item eventKey="Vegetable">Vegetable</Dropdown.Item>
                 </DropdownButton>
                 <input type="text" placeholder="Search" onChange={this.onSearch} />
                 <List items={this.props.items.filter(this.filterItem)} />
